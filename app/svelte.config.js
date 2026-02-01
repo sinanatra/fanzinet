e11aabc73ab7d05afccb5c008436afe49c724f90
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-01 16:03:17 +0100

    fix: filename error

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,17 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
-const dev = process.argv.includes('dev');
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: dev ? '' : ''
-		}
-	}
+  kit: {
+    adapter: adapter({
+      fallback: "index.html",
+    }),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH,
+    },
+  },
 };
 
 export default config;
