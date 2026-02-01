2732da1b389b90bea7cf69055b064cecce990737
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-01 15:58:56 +0100

    fix simpler build

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: dev ? '' : ''
+		}
 	}
 };
 
