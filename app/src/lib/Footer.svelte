ca132eaa5e91644a7aa2492f96db1ab8a3702e49
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-02 11:14:15 +0100

    footer

--- app/src/lib/Footer.svelte
+++ app/src/lib/Footer.svelte
@@ -3,12 +3,12 @@
     author = "Paolo Palmacci",
     authorUrl = "https://www.paolopalmacci.it/",
     developer = "Giacomo Nanni",
-    developerUrl = "https://giacomo.website/"
+    developerUrl = "https://giacomo.website/",
   } = $props();
 </script>
 
-<footer class="bg-black border-t text-xs text-white/70 px-2 py-1">
-  <div class="flex flex-wrap items-center justify-between gap-1">
+<footer class="bg-black min-h-[200px] border-t text-base text-white/70 px-2 py-1">
+  <div class="mb-4">
     <a
       href={authorUrl}
       target="_blank"
@@ -22,5 +22,32 @@
       rel="noopener noreferrer"
       class="text-blue-600 hover:text-blue-800">{developer}</a
     >
+  </div>
+  <div class="text-white text-xs max-w-[800px]">
+    <p class="mb-2">
+      Questa è una mappatura della Rete delle Fanzine italiane originate dalla
+      subcultura punk/nuovo rock negli anni ’80. Nell’Era del Telefono Fisso e
+      del Francobollo ogni fanzine era un “nodo” di una rete analogica, con un
+      indirizzo fisico che funzionava, di fatto, come un indirizzo IP.
+    </p>
+    <p class="mb-2">
+      Collage di immagini e testi scritti a mano o battuti a macchina,
+      ciclostilati o fotocopiati, che si concretizzavano in oggetti creati
+      manualmente, quasi sempre del tutto artigianali.
+    </p>
+    <p class="mb-2">
+      Questa mappatura e relativo archivio sono sviluppati nella totale aderenza
+      allo spirito DIY che le animava. È totalmente gratuita e non esiste alcun
+      ricavo oltre il Piacere di scoprire e condividere.
+    </p>
+    <p class="mb-2">
+      Chiunque volesse fornire indicazioni, informazioni, notizie, documenti,
+      suggerimenti e correzioni riguardo questa mappatura è benvenuto.
+    </p>
+    <p class="mb-2">
+      Chiunque rintracciasse su questo sito un documento sul quale può vantare
+      qualsivoglia diritto può farlo sapere: il contenuto sarà immediatamente
+      rimosso.
+    </p>
   </div>
 </footer>
